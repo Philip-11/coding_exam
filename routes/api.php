b08db4845aa1af8f7e0b7a91edbdefc3d29c44c7
Philip-11 <magsaysayphil@gmail.com>
2025-11-26 21:49:00 +0800

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Http\Controllers\Api\RolesController;
+use App\Http\Controllers\Api\UsersController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+//Login route
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    //Logout route
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    //User route
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::post('/users', [UsersController::class, 'store']);
+    Route::get('/users/{id}', [UsersController::class, 'show']);
+    Route::put('/users/{id}', [UsersController::class, 'update']);
+    Route::delete('/users/{id}', [UsersController::class, 'destroy']);
+
+    //Role route
+    Route::get('/roles', [RolesController::class, 'index']);
+    Route::post('/users', [RolesController::class, 'store']);
+    Route::get('/users/{id}', [RolesController::class, 'show']);
+    Route::put('/users/{id}', [RolesController::class, 'update']);
+    Route::delete('/users/{id}', [RolesController::class, 'destroy']);
+});
