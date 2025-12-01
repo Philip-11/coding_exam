0526a7989760e377fb9f1c999bea7557c78cc988
Philip-11 <magsaysayphil@gmail.com>
2025-12-02 00:36:31 +0800

    made a seeder for admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('roles')->insert([
+            'rolename' => 'Admin',
+            'description' => 'Has access to all of the features',
         ]);
+        
+        DB::table('users')->insert([
+            'full_name' => 'Admin User',
+            'email_address' => 'admin@example.com',
+            'nominated_pass' => 'admin123',
+            'confirmed_pass' => Hash::make('admin123'),
+            'role_id' => 1,
+        ]);
+
+        
     }
 }
