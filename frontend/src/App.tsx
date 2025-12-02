61b0b04e15ae26e848671b6c540a4f15bd6449f6
Philip-11 <magsaysayphil@gmail.com>
2025-12-03 04:28:12 +0800

    added route for create users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users/Users';
+import CreateUsers from './pages/Users/CreateUsers';
 import './App.css';
 import setAuthToken from './api/setAuthToken';
 
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={ <Login /> } />
         <Route path='/dashboard' element={ <Dashboard /> }/>
         <Route path='/users' element={ <Users /> }/>
+        <Route path='/create/users' element={ <CreateUsers /> }/>
       </Routes>
     </BrowserRouter>
   );
