28ccf8f6ce814b95017d358cb4316887421b60ab
Philip-11 <magsaysayphil@gmail.com>
2025-12-03 05:20:00 +0800

    add edit route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users/Users';
 import CreateUsers from './pages/Users/CreateUsers';
+import EditUsers from './pages/Users/EditUsers';
 import './App.css';
 import setAuthToken from './api/setAuthToken';
 
@@ -19,7 +20,8 @@ function App() {
         <Route path='/' element={ <Login /> } />
         <Route path='/dashboard' element={ <Dashboard /> }/>
         <Route path='/users' element={ <Users /> }/>
-        <Route path='/create/users' element={ <CreateUsers /> }/>
+        <Route path='/users/create' element={ <CreateUsers /> }/>
+        <Route path='/users/edit/:id' element={ <EditUsers /> }/>
       </Routes>
     </BrowserRouter>
   );
